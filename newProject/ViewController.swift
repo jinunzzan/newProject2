6342d39891a09ee4040d395f3e342525a4c96218
jinunzzan <lovebox6607@gmail.com>
2023-04-30 18:59:04 +0900

    this is new project

--- newProject/ViewController.swift
+++ newProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("커밋이 왜 안될까")
     }
 
 
